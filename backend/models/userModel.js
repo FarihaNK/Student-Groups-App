5a907735bb669b5ed6db5f92047575b525438e5d
FarihaNK <nawed.khan7904@gmail.com>
2024-01-01 21:47:25 -0500

    hash passwords

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const bcrypt = require(bcrypt)
 
 const Schema = mongoose.Schema
 const userSchema = new Schema({
@@ -11,6 +12,19 @@ const userSchema = new Schema({
         type: String,
         required: true
     },
-}, {timestamps: true})
+})
+
+//static signup method
+userSchema.static.signup = async (email, password) => {
+    const exists = await this.findOne({email})
+    if (exists){throw Error("Email already in use")}
+
+    //generate salt password3r37hr38fh3r
+    const salt = await bcrypt.genSalt(10)
+    //hash
+    const hash = await bcrypt.hash(password, salt)
+    const user = await this.create({email, password: hash})
+    return user
+}
 
 module.exports = mongoose.model("User", userSchema)

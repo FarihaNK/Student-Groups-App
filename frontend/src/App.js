a3b83971bdfd037705a8d6feedfe61487dcc6d93
FarihaNK <nawed.khan7904@gmail.com>
2024-01-01 19:24:22 -0500

    create explore page and add clubs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,53 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home';
 
 function App() {
+  // Function to create a card for each student group
+  const createCard = (title, text) => {
+    return (
+      <div className="card" style={{ width: '18rem', margin: '10px' }}>
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{text}</p>
+          <a href="#" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    );
+  };
 
-  const [studentGroups, setStudentGroups] = useState(null)
+  // Dictionary
+  const [studentGroups, setStudentGroups] = useState(null);
 
   useEffect(() => {
-    const fetchgroups = async () => {
-      const response = await fetch("/api/studentgroups")
-      const json = await response.json()
+    const fetchGroups = async () => {
+      const response = await fetch('/api/studentgroups');
+      const json = await response.json();
 
       if (response.ok) {
-        setStudentGroups(json)
+        setStudentGroups(json);
       }
-    }
+    };
 
-    fetchgroups()
-  }, [])
+    fetchGroups();
+  }, []);
 
+  // Map the student groups to create cards
+  const groupCards =
+    studentGroups &&
+    studentGroups.map((studentGroup) => {
+      return createCard(studentGroup.name, studentGroup.description);
+    });
+
+  // Return statement
   return (
     <div className="App">
       <BrowserRouter>
-      <Home/>
-      <div className='studentgroups'>
-        {studentGroups && studentGroups.map((studentGroups) => (
-          <p key={studentGroups._id}>{studentGroups.name}</p>
-        ))}
-      </div>
+        <Home />
+        <div className="studentgroups">{groupCards}</div>
       </BrowserRouter>
     </div>
   );
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,6 +4,7 @@ function Home() {
     return(
         <div className="home-container">
             <Navbar/>
+            
         </div>
     )
 }

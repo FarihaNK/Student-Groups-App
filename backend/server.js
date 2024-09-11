f6d8a24472242bfabad77594430fafeb2ca0663f
FarihaNK <nawed.khan7904@gmail.com>
2024-09-10 22:54:41 -0400

    adding cors for backend-frontend apis

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+
+const cors = require('cors');
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config()
@@ -11,6 +13,15 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+
+//docker debugging, cross-origin b/w frontend and backend
+// Configure CORS to allow requests from the frontend
+app.use(cors({
+    origin: 'http://localhost:3000', // Allow only the frontend URL
+    credentials: true // Allow credentials (optional if needed for authentication)
+}));
+app.options('*', cors()); // Enable pre-flight requests for all routes
+
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
